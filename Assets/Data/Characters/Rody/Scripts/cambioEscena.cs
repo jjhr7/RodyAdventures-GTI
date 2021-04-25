8ceabf1b24bb4ee5e992fae7da1a196c7df94d7d
belen <bgralop@epsg.upv.es>
2021-04-25 17:24:28 +0200

    Transición entre escenas

--- Assets/Data/Characters/Rody/Scripts/cambioEscena.cs
+++ Assets/Data/Characters/Rody/Scripts/cambioEscena.cs
@@ -6,11 +6,23 @@ using UnityEngine.InputSystem;
 
 public class cambioEscena : MonoBehaviour
 {
+
+    public Animator transition;
+    public float transitionTime = 1f;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene("arena_nyapos");
+            StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 2));
+
         }
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSecons(transitionTime);
+        SceneManager.LoadScene(levelIndex);
     }
 }
