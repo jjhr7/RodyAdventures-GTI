1c95d951033da6582ec0ed9b1c5b1dcfb2f4f714
belen <bgralop@epsg.upv.es>
2021-04-25 21:34:57 +0200

    Transición de play a arena 1 y arena 1 y ñapos y destrucción cajas

--- Assets/Data/Characters/Rody/Scripts/cambioEscena.cs
+++ Assets/Data/Characters/Rody/Scripts/cambioEscena.cs
@@ -6,21 +6,14 @@ using UnityEngine.InputSystem;
 
 public class cambioEscena : MonoBehaviour
 {
-    public Animator transition;
-    public float transitionTime = 5f;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag.Equals("Player"))
         {
-            StartCoroutine(LoadLevel("arena_nyapos"));
+            SceneManager.LoadScene("arena_nyapos");
         }
     }
 
-    IEnumerator LoadLevel(string levelIndex)
-    {
-        transition.SetTrigger("Start");
-        yield return new WaitForSeconds(transitionTime);
-        SceneManager.LoadScene(levelIndex);
-    }
+
 }
