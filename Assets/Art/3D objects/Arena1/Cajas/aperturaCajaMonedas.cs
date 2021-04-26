1877bdef171f2555d3a0d57c7c71da4e09111998
Belén Grande <belenchu-2001@hotmail.com>
2021-04-26 19:30:52 +0200

    Revert "Caja monedas te da 6 monedas al destruirlas"
    This reverts commit 767459eeaf9d9988a967ff1f78bf9b9c651e8c7c.

--- Assets/Art/3D objects/Arena1/Cajas/aperturaCajaMonedas.cs
+++ Assets/Art/3D objects/Arena1/Cajas/aperturaCajaMonedas.cs
@@ -4,31 +4,93 @@ using UnityEngine;
 
 public class aperturaCajaMonedas : MonoBehaviour
 {
-    PlayerStats playerStats;
+    [Range(0, 100)]
+    [SerializeField]
+    private int salud = 4;
+    public bool recibiendoDanyo;
+    double timer = 0.0;
+    public float pausa;
+    PlayerStats PlayerStats;
     GameObject[] player;
     private GameObject myplayer;
-    public EnemyStats stats;
 
+    Animator animator;
 
-    private void Update()
+    private void Awake()
     {
-        if (stats.recibiendoDanyo)
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    public int Salud
+    {
+        get { return salud; }
+        set
         {
-            Destroy(gameObject);
-            Debug.Log("hola");
-            playerStats.TakeMoney(6);
-            
+            salud = value;
+            if (salud <= 0)
+            {
+                //animator.Play("Dead_01");
+                Destroy(gameObject);
+            }
         }
     }
 
-    private void Start()
+
+    private void OnCollisionEnter(Collision collision)
     {
+        Collider bala = collision.gameObject.GetComponent<Collider>();
+        Destruirbala Dbala = collision.gameObject.GetComponent<Destruirbala>();
+        if (bala.tag.Equals("Bala"))
+        {
+            if (!recibiendoDanyo)
+            {
+                //animator.Play("Damage_01");
+                Salud -= Dbala.danyo;
+                recibiendoDanyo = true;
+            }
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        recibiendoDanyo = false;
+
         player = GameObject.FindGameObjectsWithTag("Player");
         myplayer = player[0];
-        playerStats = myplayer.GetComponent<PlayerStats>();
-        stats = gameObject.GetComponent<EnemyStats>();
+        PlayerStats = myplayer.GetComponent<PlayerStats>();
 
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (recibiendoDanyo)
+        {
+            //temporizador recibir danyo
+            timer += Time.deltaTime;
+
+            if (timer > pausa)
+            {
+                recibiendoDanyo = false;
+                timer = 0;
+            }
+        }
+
+
+    }
+
+
+    public void TakeDamage(int damage)
+    {
+        if (!recibiendoDanyo)
+        {
+            Salud -= damage;
+            recibiendoDanyo = true;
+        }
+    }
 }
 
