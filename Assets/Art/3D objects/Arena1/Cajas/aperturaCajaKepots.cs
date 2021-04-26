bdc780e8ad8c67fb2b56250fa06e279e81dd944d
John <jonnyhernandezramirez7@gmail.com>
2021-04-26 21:31:36 +0200

    Cajas Working

--- Assets/Art/3D objects/Arena1/Cajas/aperturaCajaKepots.cs
+++ Assets/Art/3D objects/Arena1/Cajas/aperturaCajaKepots.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class aperturaCajaKepots : MonoBehaviour
 {
+    EnemyStats stats;
+    public int valor = 10;
+    
+    PlayerStats playerStats;
+    GameObject[] player;
+    private GameObject myplayer;
+
+    
     // Start is called before the first frame update
     void Start()
     {
+        stats = gameObject.GetComponent<EnemyStats>();
         
+        player = GameObject.FindGameObjectsWithTag("Player");
+        myplayer = player[0];
+        playerStats = myplayer.GetComponent<PlayerStats>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (stats.recibiendoDanyo)
+        {
+            playerStats.TakeMoney(valor);
+            Destroy(gameObject);
+        }
     }
 }
