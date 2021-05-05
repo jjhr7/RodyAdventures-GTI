b66f547a28180d05111cfb3df8148ba7ac7aa026
andrey <andreykuzmin342@gmail.com>
2021-05-05 16:51:34 +0200

    Arreglado bug modo enfoque dengue al matar un enemigo

--- Assets/Data/Characters/Enemies/Común/EnemyStats.cs
+++ Assets/Data/Characters/Enemies/Común/EnemyStats.cs
@@ -3,102 +3,108 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-    public class EnemyStats : CharacterManager
+public class EnemyStats : CharacterManager
+{
+
+    [Range(0, 500)]
+    [SerializeField]
+    private int salud = 20;
+    public bool recibiendoDanyo;
+    double timer = 0.0;
+    public float pausa;
+    PlayerStats PlayerStats;
+    GameObject[] player;
+    private GameObject myplayer;
+    InputHandler inputHandler;
+    CameraHolder cameraHolder;
+
+
+
+
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    public int Salud
+    {
+        get { return salud; }
+        set
+        {
+            salud = value;
+            if (salud <= 0) //cuando muera
+            {
+                //animator.Play("Dead_01");
+                inputHandler.lockOnFlag = false; //quito el modo enfoque
+                cameraHolder.ClearLockOnTarget(); //limpio la lista de enemigos cerca que puede enfocar
+                Destroy(gameObject); //destruir objeto
+            }
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Collider bala = collision.gameObject.GetComponent<Collider>();
+        Destruirbala Dbala = collision.gameObject.GetComponent<Destruirbala>();
+        if (bala.tag.Equals("Bala"))
+        {
+            if (!recibiendoDanyo)
+            {
+                //animator.Play("Damage_01");
+                Salud -= Dbala.danyo;
+                recibiendoDanyo = true;
+            }
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        recibiendoDanyo = false;
+
+        player = GameObject.FindGameObjectsWithTag("Player");
+        myplayer = player[0];
+        PlayerStats = myplayer.GetComponent<PlayerStats>();
+        inputHandler = FindObjectOfType<InputHandler>();
+        cameraHolder = FindObjectOfType<CameraHolder>();
+        //Debug.Log("Ha entrado en TakeDamege");
+
+    }
+
+    // Update is called once per frame
+    void Update()
     {
 
-        [Range(0, 500)]
-        [SerializeField]
-        private int salud = 20;
-        public bool recibiendoDanyo;
-        double timer = 0.0;
-        public float pausa;
-        PlayerStats PlayerStats;
-        GameObject[] player;
-        private GameObject myplayer;
 
-       
-
-
-        Animator animator;
-
-        private void Awake()
+        if (recibiendoDanyo)
         {
-            animator = GetComponentInChildren<Animator>();
-    }
+            //temporizador recibir danyo
+            timer += Time.deltaTime;
 
-        public int Salud
-        {
-            get { return salud; }
-            set
+            if (timer > pausa)
             {
-                salud = value;
-                if (salud <= 0)
-                {
-                    //animator.Play("Dead_01");
-                    Destroy(gameObject);
-                }
+                recibiendoDanyo = false;
+                timer = 0;
             }
         }
 
-
-        private void OnCollisionEnter(Collision collision)
-        {
-            Collider bala = collision.gameObject.GetComponent<Collider>();
-            Destruirbala Dbala = collision.gameObject.GetComponent<Destruirbala>();
-            if (bala.tag.Equals("Bala"))
-            {
-                if (!recibiendoDanyo)
-                {
-                    //animator.Play("Damage_01");
-                    Salud -= Dbala.danyo;
-                    recibiendoDanyo = true;
-                }
-            }
-        }
-
-
-        // Start is called before the first frame update
-        void Start()
-        {
-            recibiendoDanyo = false;
-
-            player = GameObject.FindGameObjectsWithTag("Player");
-            myplayer = player[0];
-            PlayerStats = myplayer.GetComponent<PlayerStats>();
-        Debug.Log("Ha entrado en TakeDamege");
 
     }
 
-        // Update is called once per frame
-        void Update()
-        {
 
-
-            if (recibiendoDanyo)
-            {
-                //temporizador recibir danyo
-                timer += Time.deltaTime;
-
-                if (timer > pausa)
-                {
-                    recibiendoDanyo = false;
-                    timer = 0;
-                }
-            }
-
-
-        }
-
-
-        public void TakeDamage(int damage)
-        {
-        Debug.Log("Ha entrado en TakeDamege");
+    public void TakeDamage(int damage)
+    {
+        //Debug.Log("Ha entrado en TakeDamege");
         if (!recibiendoDanyo)
-            {
-                Salud -= damage;
-                recibiendoDanyo = true;
-            
-            }
+        {
+            Salud -= damage;
+            recibiendoDanyo = true;
+
         }
     }
+}
 
