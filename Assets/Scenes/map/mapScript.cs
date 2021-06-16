c4157dc389df8d5f81c3e18c1ab40b0c1c57f299
Leire <lvilmar1@epsg.upv.es>
2021-06-16 09:37:12 +0200

    Mapa casi acabado

---
+++ Assets/Scenes/map/mapScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class mapScript : MonoBehaviour
+{
+
+    public static bool mapIsOut = false;
+
+    public GameObject minimap;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.mKey.wasPressedThisFrame)
+        {
+            if (mapIsOut)
+            {
+                minimap.SetActive(false);
+            }
+            else
+            {
+                minimap.SetActive(true);
+                mapIsOut = true;
+
+            }
+
+        }
+        
+    }
+
+}
