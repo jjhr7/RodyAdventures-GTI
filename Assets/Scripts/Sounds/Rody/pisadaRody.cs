b23b18f308e1e5db754321fcfc35bb725ac84088
John <jonnyhernandezramirez7@gmail.com>
2021-06-16 18:28:34 +0200

    Música nivel de moto - nueva solución

--- Assets/Scripts/Sounds/Rody/pisadaRody.cs
+++ Assets/Scripts/Sounds/Rody/pisadaRody.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
 
 public class pisadaRody : MonoBehaviour
 {
     public AudioSource sonidoPisada;
     public void OnTriggerEnter(Collider other)
     {
+        sonidoPisada.pitch = Random.Range(0.5f,1.5f);
         sonidoPisada.Play();
     }
 }
